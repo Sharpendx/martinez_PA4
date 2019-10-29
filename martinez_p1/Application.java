936ec79b990d24ca72cb8744820264dd456e83e0
Sharpendx <albertomartinez@knights.ucf.edu>
2019-10-29 18:41:04 -0400

    Mostly working but can't get the duplicates out

--- martinez_p1/Application.java
+++ martinez_p1/Application.java
@@ -1,15 +1,27 @@
-
 import java.io.File;
+import java.util.Arrays;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
 
 public class Application
 {
 
 
-public static void main(String args[])
+public static void main (String[] args) throws IOException
     {
-      File myObj = new File("filename.txt");
       DuplicateRemover dupRemove = new DuplicateRemover();
+      File f = new File("unique_words.txt");
+    //  File b = new File("problem1.txt");
+      f.createNewFile();
+      //fileCheck(f);
+      //fileCheck(b);
+
+      dupRemove.remove("problem1.txt");
+
+      dupRemove.write("unique_words.txt");
 
     }
 
@@ -19,7 +31,22 @@ public static void print(String s)
     {
       System.out.print(s);
     }
+public static void fileCheck(File f)
+{
+  if (f.exists())
+  {
+      System.out.println("File name: " + f.getName());
+      System.out.println("Absolute path: " + f.getAbsolutePath());
+      System.out.println("Writeable: " + f.canWrite());
+      System.out.println("Readable " + f.canRead());
+      System.out.println("File size in bytes " + f.length());
+  }
 
+  else
+  {
+    System.out.println("The file does not exist.");
+  }
+}
 public static void println(String s)
     {
       System.out.println(s);
--- martinez_p1/DuplicateRemover.java
+++ martinez_p1/DuplicateRemover.java
@@ -1,10 +1,8 @@
 /*
 Create a utility class called DuplicateRemover.
-Create an instance method called remove that takes a single parameter called dataFile (representing the path to a text file) and uses a
-Set of Strings to eliminate duplicate words from dataFile.
+
 The unique words should be stored in an instance variable called uniqueWords.
-Create an instance method called write that takes a single parameter called outputFile (representing the path to a text file) and writes
-the words contained in uniqueWords to the file pointed to by outputFile. The output file should be overwritten if it already exists, and created if it does not exist.
+
 
 Create a separate class called Application that contains a main method which illustrates the use of DuplicateRemover
 by calling both the remove and write methods. Your input file must be called problem1.txt and your output file must be called unique_words.txt.
@@ -12,23 +10,87 @@ by calling both the remove and write methods. Your input file must be called pro
 */
 import java.io.File;
 import java.util.Arrays;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 
 public class DuplicateRemover
 {
   private static String[] uniqueWords = new String[100];
-  private static String holdWord;
+  private static String[] uniqueWordsCheck = new String[100];
+  private static String holdWord = null;
+  private static Scanner scr = null;
+  private static FileInputStream fileByteStream = null;
+  private static FileOutputStream fileOutStream = null;
+  private static int i;
+  private static PrintWriter writer = null;
+  private static boolean isSame = false;
 
-  public static String getUniqueWords(int address)
+  //Create an instance method called remove that takes a single parameter called dataFile (representing the path to a text file) and uses a
+  //Set of Strings to eliminate duplicate words from dataFile.
+  public void remove(String dataFile) throws FileNotFoundException, IOException
   {
-    return uniqueWords[address];
+    fileByteStream = new FileInputStream(dataFile);
+    scr = new Scanner(fileByteStream);
+    i = 0;
+     while (scr.hasNextLine())
+     {
+       holdWord = scr.nextLine();
+       if(i == 0)
+       {
+        uniqueWords[i] = holdWord;
+      }
+       else
+       {
+         System.out.println("Running the else");
+         int s = i;
+         while(s > 0)
+         {
+           if(holdWord == uniqueWords[s - 1])
+           {
+             isSame = true;
+           }
+           s--;
+         }
+
+         if(isSame)
+         {
+           uniqueWords[i] = null;
+           isSame = false;
+         }
+         else
+         {
+           uniqueWords[i] = holdWord;
+         }
+       }
+       //uniqueWordsCheck[i] = holdWord;
+       i++;
+     }
+     fileByteStream.close();
   }
-  public static void remove(File dataFile)
+//Create an instance method called write that takes a single parameter called outputFile (representing the path to a text file) and writes
+//the words contained in uniqueWords to the file pointed to by outputFile. The output file should be overwritten if it already exists, and created if it does not exist.
+  public void write(String outputFile) throws FileNotFoundException, IOException
   {
-    System.out.print("It worked?");
-  }
+    fileOutStream = new FileOutputStream(outputFile);
+    writer = new PrintWriter(fileOutStream);
 
-  public static void write(File outputFile)
-  {
+    for(int k = 0; k < uniqueWords.length ; k++)
+    {
+      if(uniqueWords[k] == null)
+      {
+        continue;
+      }
+      else
+      {
+      writer.println(uniqueWords[k]);
+      }
+    }
+    writer.flush();
 
+    fileByteStream.close();
   }
 }
