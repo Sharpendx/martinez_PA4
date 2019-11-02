c930d39cd7569a004124c85edd6abe05bb6b4f6b
Sharpendx <albertomartinez@knights.ucf.edu>
2019-11-02 11:51:23 -0400

    DONE problem 2

--- martinez_P2/Application.java
+++ martinez_P2/Application.java
@@ -1,5 +1,7 @@
 //Create a separate class called Application that contains a main method which illustrates the use of DuplicateCounter
 //by calling both the remove and write methods. Your input file must be called problem2.txt and your output file must be called unique_word_counts.txt.
+import java.io.*;
+import java.util.*;
 import java.io.File;
 import java.util.Arrays;
 import java.io.FileInputStream;
@@ -12,7 +14,14 @@ public class Application
 {
   public static void main (String[] args) throws IOException
     {
+      DuplicateCounter dupCounter = new DuplicateCounter();
+
       File f = new File("unique_word_counts.txt");
+
       f.createNewFile();
+
+      dupCounter.count("problem2.txt");
+
+      dupCounter.write("unique_word_counts.txt");
     }
 }
--- martinez_P2/DuplicateCounter.java
+++ martinez_P2/DuplicateCounter.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.*;
 import java.io.File;
 import java.util.Arrays;
 import java.io.FileInputStream;
@@ -5,22 +7,45 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.PrintWriter;
 import java.io.FileOutputStream;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class DuplicateCounter
 {
   //instance variable called wordCounter
-  private static int wordCounter = 0;
+  //private static int wordCounter = 0;
+  private static String holdWord;
+  private static int holdVal;
+  private static int i = 0;
   private static Scanner scr = null;
   private static FileInputStream fileByteStream = null;
   private static FileOutputStream fileOutStream = null;
   private static PrintWriter writer = null;
+  Map<String, Integer> wordCounter = new HashMap<String, Integer>();
+
   //Create an instance method called count that takes a single parameter called dataFile
   //uses a Map of Strings to count how many times each word occurs in dataFile.
   public void count(String dataFile) throws FileNotFoundException, IOException
   {
     fileByteStream = new FileInputStream(dataFile);
     scr = new Scanner(fileByteStream);
+
+    while (scr.hasNextLine())
+    {
+      holdWord = scr.nextLine();
+      if(wordCounter.containsKey(holdWord) && i > 0)
+      {
+        holdVal = wordCounter.get(holdWord);
+        wordCounter.put(holdWord,holdVal + 1);
+      }
+      else
+      {
+        wordCounter.put(holdWord, 1);
+        i++;
+      }
+    }
+    fileByteStream.close();
+
   }
   //Create an instance method called write that takes a single parameter called outputFile
   //writes the contents of wordCounter to the file pointed to by outputFile.
@@ -28,5 +53,13 @@ public class DuplicateCounter
   {
     fileOutStream = new FileOutputStream(outputFile);
     writer = new PrintWriter(fileOutStream);
+
+    for (Map.Entry<String, Integer> entry : wordCounter.entrySet())
+    {
+        writer.println(entry.getKey() + " " + entry.getValue());
+    }
+    writer.flush();
+
+    fileByteStream.close();
   }
 }
